13c727a227fb903111c2a8987fa93da52a365da5
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2024-06-01 21:06:50 -0600

    ezpz command line program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace dotNet_learning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("WHat is your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
